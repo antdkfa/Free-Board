4e8fdf0c319067c6eacabf9a017d52c22de3d378
baegjaesang <ton25355!!>
2022-03-29 22:41:11 +0900

    :construction: post용 service, controller, repository 영역 생성

---
+++ src/main/java/com/jaysang/board/springboot/service/posts/PostsService.java
@@ -0,0 +1,20 @@
+package com.jaysang.board.springboot.service.posts;
+
+import com.jaysang.board.springboot.domain.posts.PostsRepository;
+import com.jaysang.board.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
---
+++ src/main/java/com/jaysang/board/springboot/web/PostsApiController.java
@@ -0,0 +1,20 @@
+package com.jaysang.board.springboot.web;
+
+import com.jaysang.board.springboot.service.posts.PostsService;
+import com.jaysang.board.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+}
